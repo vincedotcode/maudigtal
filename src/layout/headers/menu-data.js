cefb80552ca8a25fc87fe92f960f34f9b4efcf36
AzureAD\VinceErkadoo <vince@zapproach.com>
2023-10-26 17:02:30 +0400

    Initial commit with my project files

---
+++ src/layout/headers/menu-data.js
@@ -0,0 +1,45 @@
+const menu_data = [
+  {
+    id:1,
+    mega_menu:false,
+    has_dropdown:true,
+    title:'Home',
+    link:'/',
+    sub_menus:[
+     
+    ]
+  },
+  {
+    id:2,
+    mega_menu:false,
+    has_dropdown:true,
+    title:'Portfolio',
+    link:'/portfolio',
+   
+  },
+  {
+    id:3,
+    mega_menu:true,
+    has_dropdown:true,
+    title:'Pages',
+    link:'#',
+   
+  },
+  {
+    id:4,
+    mega_menu:false,
+    has_dropdown:true,
+    title:'Blog',
+    link:'/blog',
+   
+  },
+  {
+    id:5,
+    mega_menu:false,
+    has_dropdown:false,
+    title:'Contact',
+    link:'/contact',
+  }
+]
+export default menu_data;
+
---
+++ src/layout/index.jsx
@@ -0,0 +1,10 @@
+import Wrapper from "./wrapper";
+import Header from "./headers/header";
+import Footer from "./footers/footer";
+
+export {
+  Wrapper,
+  Header,
+  Footer,
+
+}
